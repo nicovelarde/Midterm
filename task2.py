9f29f28e923db0601e0ddffd2802b382bb799dcf
loctungsu <56486857+loctungsu@users.noreply.github.com>
2025-03-30 17:55:47 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,21 +1,23 @@
 from RegressionTree import RegressionTree
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 import time
 
-def generate_data(samples, limit_up, limit_dwn, seed=42):
+def generate_data(samples, limit_dwn, limit_up, seed=42):
     np.random.seed(seed)
-    x = np.random.uniform(limit_dwn, limit_up,samples).reshape(-1,1)
-    y = 0.8 * np.sin(x - 1)
-    return x , y
+    X = np.random.uniform(limit_dwn, limit_up,samples).reshape(-1,1)
+    y = 0.8 * np.sin(X - 1)
+    return X , y
 
-X,y = generate_data(100,3,-3,42)
+X,y = generate_data(100,-3,3,42)
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, train_size=0.8, random_state=42)
 
 """
 test_result
     - time cost of building the tree (time python) 
-    - test error (MSE)
+    - Normal test error (MSE)
+    - Scikit learn test error (MSE)
     - height of the regression tree
 """
 
@@ -29,85 +31,58 @@ start_time = time.time()
 tree = RegressionTree(X_train, y_train)
 stop_time = time.time()
 total_time = stop_time-start_time
-print(f"The time cost of building the tree is {total_time:.4f}")
 
 y_pred = tree.predict(X_test)
-mse = np.mean((y_test - y_pred)**2)
-print(f"The test error is {mse:.4f}")
+normal_mse = np.mean((y_test - y_pred)**2)
+scikit_learn_mse = mean_squared_error(y_test, y_pred)
 
 height = tree.height()
-print(f"The height of the resulting regression tree is {height}")
 
+print(f"\n No Limitation:")
+print(f"  - Tree Height: {height}")
+print(f"  - Scikit learn test error: {scikit_learn_mse:.5f}")
+print(f"  - Normal mse test error: {normal_mse:.5f}")
+print(f"  - Build Time: {total_time:.5f} sec")
 # Task 2.2
-print("================================================Task 2.2 ==================================\n")
-print("======================Using 1/2 height=====================================\n")
-start_time = time.time()
-tree2=RegressionTree(X_train,y_train,max_depth=7, control_by='depth')
-stop_time = time.time()
-total_time = stop_time-start_time
-print(f"The time cost of building the tree with 1/2 height is {total_time:.4f}")
+print("================================================Task 2.2 ==================================")
+for fraction in [0.5, 0.75]:
+    max_height = int(height*fraction)
 
-y_pred = tree2.predict(X_test)
-mse = np.mean((y_test - y_pred)**2)
-print(f"The test error is {mse:.4f}")
+    start_time = time.time()
+    tree=RegressionTree(X_train,y_train,max_depth=max_height, control_by='depth')
+    stop_time = time.time()
+    total_time = stop_time-start_time
 
-height = tree2.height()
-print(f"The height of the resulting regression tree is {height}")
+    y_pred = tree.predict(X_test)
+    normal_mse = np.mean((y_test - y_pred) ** 2)
+    scikit_learn_mse = mean_squared_error(y_test, y_pred)
 
-print("======================Using 3/4 height=====================================\n")
-start_time = time.time()
-tree3=RegressionTree(X_train,y_train,max_depth=(14*(3/4)), control_by='depth')
-stop_time = time.time()
-total_time = stop_time - start_time
-print(f"The time cost of building the tree with 3/4 height is {total_time:.4f}")
+    curr_height = tree.height()
 
-y_pred = tree3.predict(X_test)
-mse = np.mean((y_test - y_pred)**2)
-print(f"The test error is {mse:.4f}")
+    print(f"\n Limited Height ({fraction * 100:.0f}% of full height):")
+    print(f"  - Max Height: {curr_height}")
+    print(f"  - Scikit learn test error: {scikit_learn_mse:.5f}")
+    print(f"  - Normal mse test error: {normal_mse:.5f}")
+    print(f"  - Build Time: {total_time:.5f} sec")
 
-height2 = tree3.height()
-print(f"The height of the resulting regression tree is {height2}")
 
 #Task 2.3
-print("================================================Task 2.3 ==================================\n")
-print("======================Using a leaf size limit of 2 =====================================\n")
-start_time = time.time()
-tree4 = RegressionTree(X_train,y_train,min_samples_leaf=2,control_by='leaf')
-stop_time=time.time()
-total_time=stop_time - start_time
-print(f"The time cost of building the tree with a limit of 2 in leaf size is {total_time:.4f}")
+print("================================================Task 2.3 ==================================")
+for leaf_size in [2, 4, 8]:
+    start_time = time.time()
+    tree = RegressionTree(X_train, y_train, leaf_size, control_by='leaf')
+    stop_time = time.time()
+    total_time = stop_time - start_time
 
-y_pred = tree4.predict(X_test)
-mse = np.mean((y_test - y_pred)**2)
-print(f"The test error is {mse:.4f}")
+    y_pred = tree.predict(X_test)
+    normal_mse = np.mean((y_test - y_pred) ** 2)
+    scikit_learn_mse = mean_squared_error(y_test, y_pred)
 
-height = tree4.height()
-print(f"The height of the resulting tree is {height}")
+    curr_height = tree.height()
 
-print("======================Using a leaf size limit of 4 =====================================\n")
-start_time = time.time()
-tree5 = RegressionTree(X_train,y_train,min_samples_leaf=4,control_by='leaf')
-stop_time=time.time()
-total_time=stop_time - start_time
-print(f"The time cost of building the tree with a limit of 4 in leaf size is {total_time:.4f}")
+    print(f"\n Limited Leaf Size ({leaf_size} per leaf):")
+    print(f"  - Max Height: {curr_height}")
+    print(f"  - Scikit learn test error: {scikit_learn_mse:.5f}")
+    print(f"  - Normal mse test error: {normal_mse:.5f}")
+    print(f"  - Build Time: {total_time:.5f} sec")
 
-y_pred = tree5.predict(X_test)
-mse = np.mean((y_test - y_pred)**2)
-print(f"The test error is {mse:.4f}")
-
-height = tree5.height()
-print(f"The height of the resulting tree is {height}")
-
-print("======================Using a leaf size limit of 8 =====================================\n")
-start_time = time.time()
-tree6 = RegressionTree(X_train,y_train,min_samples_leaf=8,control_by='leaf')
-stop_time=time.time()
-total_time=stop_time - start_time
-print(f"The time cost of building the tree with a limit of 8 in leaf size is {total_time:.4f}")
-
-y_pred = tree6.predict(X_test)
-mse = np.mean((y_test - y_pred)**2)
-print(f"The test error is {mse:.4f}")
-
-height = tree6.height()
-print(f"The height of the resulting tree is {height}")
